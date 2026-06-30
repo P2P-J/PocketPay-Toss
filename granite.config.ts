7f83ad11daf156870d908e85413f615a51449f33
Aen <olnn712@gmail.com>
2026-06-30 11:26:45 +0900

    chore(출시): 앱 아이콘 URL 설정 (배포 블로커 해소)

--- granite.config.ts
+++ granite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
       brand: {
         displayName: '작은 모임',
         primaryColor: '#3DD598', // 브랜드 메인 그린
-        icon: '', // TODO: 콘솔에 아이콘 업로드 후 받은 URL로 교체 (배포 전 필수)
+        icon: 'https://static.toss.im/appsintoss/14613/a6a86854-9796-44da-b5aa-e32a052883cf.png',
       },
       permissions: [
         { name: 'camera', access: 'access' }, // 영수증 촬영(openCamera)
